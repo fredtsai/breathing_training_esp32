b007883c448d63bb7e0663864c2bbe8ed8abf4a7
fredtsai <fred412037@gmail.com>
2020-01-12 22:27:29 +0800

    modify for send hex value to app

--- BLE_TX_RX/BLE_TX_RX.ino
+++ BLE_TX_RX/BLE_TX_RX.ino
@@ -32,7 +32,7 @@ class MyCallBack: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
       if ((rxValue.length() > 0) && (rxValue.find ('&') == std::string::npos)) {
-          Serial.printf("*** Receive data: %d ***\n", rxValue[0]);
+          Serial.printf("*** Receive data: %d %d ***\n", rxValue[0], rxValue[1]);
       }
     }
 
@@ -83,18 +83,36 @@ void loop() {
     // notify changed value
     if (deviceConnected) {
         adcValue = analogRead(potPin);
-        Serial.printf("*** ADC GPIO34: %d ***\n", adcValue);
-
+        Serial.printf("*** ADC GPIO34: %x ***\n", adcValue);
+        /*
         std::ostringstream adcValue_str;
         adcValue_str << adcValue;
         std::string converted(adcValue_str.str());
         
         std::string str1 = "20201116 2014 ";
         std::string str2 = adcValue_str.str();
-        std::string space = " ";
-        std::string str = str1 + str2 + space;
+        std::string str = str1 + str2;
+        */
+        char a[8];
+        a[0] = 20 + 1;
+        a[1] = 20 + 1; //year 2020
+        a[2] = 01 + 1;
+        a[3] = 12 + 1; //date 0112
+        a[4] = 21 + 1;
+        a[5] = 31 + 1; //time 21:31
+
         
-        pCharacteristic->setValue(str);
+        a[6] = (adcValue >> 8) + 1;
+        if(adcValue & 0x00ff == 0xff){
+          a[7] = 0xff; //pressure
+        }
+        else {
+          a[7] = (adcValue & 0x00ff) + 1; //pressure
+        }
+        
+        Serial.printf("***  %x  %x***\n", a[6], a[7]);
+        
+        pCharacteristic->setValue(a);
         //pCharacteristic->setValue(adcValue);
         pCharacteristic->notify();
         delay(500); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 500ms
